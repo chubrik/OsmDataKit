74c6ac18e93882171f549646e5b73283b953e960
Arkadi Chubrik <arkadi@chubrik.org>
2018-08-18 00:55:33 +0300

    Tests & dependencies

---
+++ OsmDataKit.Tests/Tests.cs
@@ -0,0 +1,43 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OsmDataKit.Services;
+using OsmSharp;
+
+namespace OsmDataKit.Tests
+{
+    [TestClass]
+    public class Tests : TestsBase
+    {
+        private const string SrcPath = "../$data/antarctica.osm.pbf";
+
+        [TestMethod]
+        public void LoadPredicate()
+        {
+            TestInitialize(nameof(LoadPredicate));
+            var response = OsmService.Load(SrcPath, i => i.Tags.ContainsKey("place"));
+            Assert.IsTrue(response.Nodes.Count > 100);
+            Assert.IsTrue(response.Ways.Count > 5000);
+            Assert.IsTrue(response.Relations.Count > 1000);
+            Assert.IsTrue(response.MissedNodeIds.Count == 0);
+            Assert.IsTrue(response.MissedWayIds.Count == 0);
+            Assert.IsTrue(response.MissedRelationIds.Count == 0);
+        }
+
+        [TestMethod]
+        public void LoadRelationObject()
+        {
+            TestInitialize(nameof(LoadRelationObject));
+
+            // https://www.openstreetmap.org/relation/2969204
+            var title = "Vega Island";
+            var osmId = 2969204;
+
+            var relation = OsmObjectService.LoadRelationObject(SrcPath, cacheName: title, relationId: osmId);
+            Assert.IsTrue(relation.Type == OsmGeoType.Relation);
+            Assert.IsTrue(relation.Id == osmId);
+            Assert.IsTrue(relation.Title == title);
+            Assert.IsTrue(relation.Tags["type"] == "multipolygon");
+            Assert.IsTrue(relation.Tags["place"] == "island");
+            Assert.IsTrue(relation.Members.Count > 50);
+        }
+    }
+}
---
+++ OsmDataKit.Tests/TestsBase.cs
@@ -0,0 +1,35 @@
+﻿using Kit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace OsmDataKit.Tests
+{
+    public abstract class TestsBase
+    {
+        protected const string ProjectNativePath = "../../..";
+
+        [TestInitialize]
+        public void BaseInitialize()
+        {
+            Kit.Kit.Setup(test: true, baseDirectory: ProjectNativePath);
+            ConsoleClient.Setup(minLevel: LogLevel.Log);
+        }
+
+        public void TestInitialize(string testName)
+        {
+            var workingDir = "$work/" + testName;
+            Kit.Kit.Setup(workingDirectory: workingDir);
+            ConsoleClient.Setup(minLevel: LogLevel.Log);
+
+            var nativeWorkingDir = ProjectNativePath + "/" + workingDir;
+
+            if (Directory.Exists(nativeWorkingDir))
+            {
+                foreach (var file in Directory.GetFiles(nativeWorkingDir))
+                    File.Delete(file);
+
+                Assert.IsTrue(Directory.GetFiles(nativeWorkingDir).Length == 0);
+            }
+        }
+    }
+}
