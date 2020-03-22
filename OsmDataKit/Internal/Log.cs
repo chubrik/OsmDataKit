bb391e603200d0813649e92ed223f0b63646c9ee
Arkadi Chubrik <arkadi@chubrik.org>
2020-03-22 20:41:13 +0300

    3.0.1

--- OsmDataKit/Internal/Log.cs
+++ OsmDataKit/Internal/Log.cs
@@ -1,13 +1,16 @@
-﻿using Kit;
-using System;
+﻿using System;
+
+#if !DEBUG
+using System.Runtime.CompilerServices;
+#endif
 
 namespace OsmDataKit.Internal
 {
     internal static class Log
     {
 #if DEBUG
-        public static void Debug(string message) => LogService.Log(message);
-        public static void Debug(string message, Action block) => LogService.Log(message, block);
+        public static void Debug(string message) => Kit.LogService.Log(message);
+        public static void Debug(string message, Action block) => Kit.LogService.Log(message, block);
 #else
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Debug(string message) { }
